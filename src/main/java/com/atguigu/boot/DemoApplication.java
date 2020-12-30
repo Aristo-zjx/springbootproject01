fe100dca199210d8456c8f5a136c292942722159
Aristo-zjx <1459376624@qq.com>
2020-12-30 16:41:21 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/boot/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.atguigu")
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
